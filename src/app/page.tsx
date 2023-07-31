8c116adfd0dd6347d7824db7216844f00412b1c2
Imraan Iqbal <imraan1901@gmail.com>
2023-07-31 13:28:39 -0700

    Working Website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,107 @@
-import Image from 'next/image'
+'use client';
+import { useState } from "react";
+import { v4 } from "uuid";
 
-export default function Home() {
+const App = () => {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [filename, setFileName] = useState("");
+  const [auth, setAuth] = useState(false);
+  const [myText, setMyText] = useState("Click To Check Employee Status");
+
+  // This function will be triggered when the file field changes
+  const imageChange = (e: any) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setSelectedImage(e.target.files[0]);
+      setAuth(false)
+      setMyText("Click To Check Employee Status")
+      setFileName(v4())
+    }
+  };
+
+  async function submitToBackend() {
+    setMyText(`Checking...`)
+    let filenameQuery = encodeURIComponent(filename)
+    let url = `https://2p47twxmsj.execute-api.us-west-1.amazonaws.com/dev/auth?filename=${filenameQuery}`
+    const backendResponse = await fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "image/*",
+      },
+      body: selectedImage,
+    })
+    //setName(await backendResponse.json())
+    let resp = await backendResponse.json()
+    console.log(resp)
+    if (resp['Authenticated'] == true) {
+      setAuth(true)
+      setMyText(`Welcome ${resp['message'][0]} ${resp['message'][1]}!`)
+    }
+    else {
+      setAuth(false)
+      setMyText(`Employee not found`)
+    }
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <>
+      <div style={styles.container as React.CSSProperties}>
+        <h1 style={{"height" : "100px", "fontSize" : "30px", "alignItems": "center",}}>
+          Employee Face Recognition App</h1>
+        <h2 style={{"height" : "150px", "width" : "450px", "justifyContent" : "center"}}>I have added Cillian Murphy, Emily Blunt, Robert Downey-Jr., 
+          Margot Robbie, Ryan Gosling, and Will Ferrell. Go ahead and find
+          a jpg, jpeg, or png file of anyone one of those people to see if they are recognized.
+          Also you can upload someone not in this list to see if they are not recognized.</h2>
+        <input style={{"alignItems": "center", "justifyContent" : "center" }}
+          accept="image/jpg, image/jpeg, image/png"
+          type="file"
+          onChange={imageChange}
         />
+
+        {selectedImage && (
+          <div style={styles.preview as React.CSSProperties}>
+            <button onClick={submitToBackend} style={auth ? styles.Approved : styles.Denied}>
+              {myText}
+            </button>
+            <img
+              src={URL.createObjectURL(selectedImage)}
+              style={styles.image}
+            />
+          </div>
+        )}
       </div>
+    </>
+  );
+};
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+export default App;
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+// Just some styles
+const styles = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    paddingTop: 50,
+  },
+  preview: {
+    marginTop: 50,
+    display: "flex",
+    flexDirection: "column",
+  },
+  image: { maxWidth: 500, maxHeight: 500 },
+  Approved: {
+    cursor: "pointer",
+    padding: 15,
+    background: "Green",
+    color: "white",
+    border: "none",
+  },
+  Denied: {
+    cursor: "pointer",
+    padding: 15,
+    background: "Red",
+    color: "white",
+    border: "none",
+  },
+};
